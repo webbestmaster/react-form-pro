cf41854d0e8921d66d9de3733de90e1d41eb563c
dim <web.best.master@gmail.com>
2020-11-01 20:53:18 +0300

    WIP

--- flow-typed/react-form-pro.js
+++ flow-typed/react-form-pro.js
@@ -67,7 +67,7 @@ declare module 'react-form-pro' {
     |};
 
     // eslint-disable-next-line id-match
-    declare export var Table: React$ComponentType<FormPropsType>;
+    declare export var Form: React$ComponentType<FormPropsType>;
 }
 
 declare module 'react-form-pro/dist/style.css' {
