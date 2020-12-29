7e3ceb32201de7a345a28db793670d7f5bb35b2e
dim <web.best.master@gmail.com>
2020-12-29 11:17:56 +0300

    refactor

--- webpack/setting/resolve/alias.js
+++ webpack/setting/resolve/alias.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const {cwd} = require('../../config');
 
-const duplicateList = ['regenerator-runtime'];
+const duplicateList = [];
 
 module.exports.alias = duplicateList.reduce((accumulator, packageName) => {
     return {...accumulator, [packageName]: path.resolve(cwd, `node_modules/${packageName}`)};
